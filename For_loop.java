20946cb2342c830b3ad871d6a4c2b81929930bb6
utsavbabariya811 <24it003@charusat.edu.in>
2025-10-06 20:24:14 +0530

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,10 @@
+
+public class For_Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
