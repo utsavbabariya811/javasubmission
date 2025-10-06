a585e91bdd41c60c25a0938d5e9dd09626bf9f7b
utsavbabariya811 <24it003@charusat.edu.in>
2025-10-06 20:29:59 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,23 @@
+package Java_Self_Study;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
