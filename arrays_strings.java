4f7f8f4c014ebe167b63aa95076200fce7aa3312
utsavbabariya811 <24it003@charusat.edu.in>
2025-10-06 20:26:27 +0530

    Create arrays_strings.java

---
+++ arrays_strings.java
@@ -0,0 +1,14 @@
+
+public class arrays_strings {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+        String text = "Hello Java";
+        System.out.println(text);
+        System.out.println(text.length());
+        System.out.println(text.toUpperCase());
+    }
+}
